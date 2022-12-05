6795e1c922efe142c7a15c6e3ead1ffe391291b6
Haider Zulfiqar <haiderbhinder10@gmail.com>
2022-12-05 21:26:53 +0500

    Add association to UI

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end  
 
   def index
@@ -23,6 +24,20 @@ class CategoriesController < ApplicationController
       render 'new'
     end  
   end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category Name updated successfuly"
+      redirect_to @category
+    else
+      render 'edit'
+    end    
+  end  
 
   private
   def category_params
